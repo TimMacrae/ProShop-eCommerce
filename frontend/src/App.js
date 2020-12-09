19515412254b0175a442ca461cefce1fca5a3788
TimBreuel <55285070+TimBreuel@users.noreply.github.com>
2020-12-09 14:34:53 +0100

    update products and artical

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,15 @@ import "./App.css";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <div className="App">
       <Header></Header>
-      <main>
+      <main className="py-5">
         <Container>
-          <h1>Welcome to the Shop</h1>
+          <HomeScreen></HomeScreen>
         </Container>
       </main>
       <Footer></Footer>
