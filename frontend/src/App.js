ce6cb69a12c4317e23be312800efd881573f5e09
TimBreuel <55285070+TimBreuel@users.noreply.github.com>
2020-12-09 11:43:20 +0100

    add bootstrap / watch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <main>
+        <Container>
+          <h1>Welcome to the Shop</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default App;
