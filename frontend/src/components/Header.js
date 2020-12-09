12be24f3c857feafd59fe66854ea8f4b2f9f8713
TimBreuel <55285070+TimBreuel@users.noreply.github.com>
2020-12-09 12:12:02 +0100

    add header, footer and nav

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,15 +3,21 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/cart">Cart</Nav.Link>
-            <Nav.Link href="/login">Sign In</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart  pr-1"></i> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user pr-1"></i>Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
     </header>
   );
